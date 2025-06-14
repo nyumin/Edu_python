224bc1e8dc06bea9e834b6e65e0c345de3d42a75
sh6429111@naver.com <doomdex84@gmail.com>
2025-06-14 16:46:33 +0900

    수업내용

---
+++ ARGV/main.py
@@ -0,0 +1,5 @@
+# sys라는 python package,module를 import
+
+import sys
+name = sys.argv[1]
+print("저의 이름은 " + name + "입니다.")
---
+++ ARGV/main2.py
@@ -0,0 +1,9 @@
+import sys
+
+first_name = sys.argv[1]
+last_name = sys.argv[2]
+gender = sys.argv[3]
+
+print("성은 " + first_name + "입니다.")
+print("이름은 " + last_name + "입니다.")
+print("성별은 " + gender + "입니다.")
